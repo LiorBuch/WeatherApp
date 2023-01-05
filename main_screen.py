ff33b50ffd1d08ae37c2d46214117c233de984ec
liorb <lior.buchmeier@gmail.com>
2023-01-05 16:56:11 +0200

    read for deploy

--- main_screen.py
+++ main_screen.py
@@ -11,6 +11,7 @@ from kivymd.uix.textfield import MDTextField
 
 class MainScreen(Screen):
     def __init__(self, **kw):
+        self.api_key = "a4Gsee6jhV2Klc89Prvsh34"
         super().__init__(**kw)
         self.name = "main_screen"
         self.main_layout = FloatLayout()
@@ -20,14 +21,17 @@ class MainScreen(Screen):
                                       pos_hint={'top': 0.95, 'left': 0.1})
         self.search_btn = MDFlatButton(text="Search Weather", on_press=self.search_btn_click,
                                        pos_hint={'right': 0.95, 'bottom': 0.95})
-        self.city_name = MDLabel(text="no")
+        self.city_name = MDLabel(text="")
         self.temp = MDLabel(text="0")
+        self.sky_status = MDLabel(text="sky state")
         self.description = MDLabel(text="description")
         self.min_temp = MDLabel(text="0")
         self.max_temp = MDLabel(text="0")
 
         self.main_layout.add_widget(self.stats_layout)
         self.stats_layout.add_widget(self.city_name)
+        self.stats_layout.add_widget(self.sky_status)
+        self.stats_layout.add_widget(self.description)
         self.stats_layout.add_widget(self.temp)
         self.stats_layout.add_widget(self.max_temp)
         self.stats_layout.add_widget(self.min_temp)
@@ -44,17 +48,18 @@ class MainScreen(Screen):
             pop.open()
             return
         try:
+            city_name = self.search_box.text
             weather_pack = requests.get(
-                url=f"https://weathergrab.azurewebsites.net/api/geocoding?city={self.search_box.text}")
+                url=f"https://gittester.azurewebsites.net/weather/city={city_name}&key={self.api_key}")
             if weather_pack.status_code == 200:
                 data = weather_pack.json()
-                self.sky_icon.source = f'http://openweathermap.org/img/wn/{data}@2x.png'  # Todo add data nest
-                self.temp.text = data  # Todo add data nest
-                self.min_temp.text = data  # Todo add data nest
-                self.max_temp.text = data  # Todo add data nest
-                self.description.text = data  # Todo add data nest
-                data = weather_pack.json()
-                print(data)
+                self.sky_icon.source = f'http://openweathermap.org/img/wn/{str(data["state"]["sky_icon"])}@2x.png'
+                self.temp.text = "Current Temperature:" + str(data['temp']['current'])
+                self.min_temp.text = "Minimum Temperature Today:" + str(data['temp']['min'])
+                self.max_temp.text = "Maximum Temperature Today:" + str(data['temp']['max'])
+                self.city_name.text = str(data['city_name'])
+                self.sky_status.text = str(data['state']['sky'])
+                self.description.text = str(data['state']['sky_info'])
             else:
                 pop = Popup(title="Connection Error")
                 pop.open(size_hint=(0.4, 0.4))
